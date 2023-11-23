6336f433b902c894aa39be54f49307f39b90f4bc
sawiedwin <edwin.quinterogu@campusucc.edu.co>
2023-11-22 21:53:47 -0500

    ahora si

--- src/screens/Habilidades.tsx
+++ src/screens/Habilidades.tsx
@@ -1,25 +1,25 @@
+import { useTranslation } from 'react-i18next';
 import { FaPython, FaJava, FaJs, FaAngular, FaReact, FaFigma, FaCss3Alt } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa6';
-import { useTranslation } from 'react-i18next';
 
 
 const backgroundImageUrl = 'url("src/assets/alto-angulo-red-comunicacion-internet-espacio-copia.jpg")';
 
 const Habilidades = () => {
+  const {t} = useTranslation();
 
-  const {t}= useTranslation();
   const iconColor = '#f6403d';
-  
   const sectionStyle = {
     backgroundImage: backgroundImageUrl,
     backgroundSize: 'cover',
+    backgroundPosition: 'center',
     height: '100vh',
-    backgroundColor: 'rgba(255)' 
+    background: 'rgba(0, 0, 0, 0.75) url("src/assets/alto-angulo-red-comunicacion-internet-espacio-copia.jpg") center/cover no-repeat',
   };
   return (
-    <section style={sectionStyle} className='flex flex-col items-center justify-center h-full space-y-4 bg-cover bg-center bg-opacity-75'>
-      <h1 className='text-red-500 text-4xl md:text-6xl font-semibold mb-4'>{t('parrafohabil')}</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <section style={sectionStyle} className='flex flex-col items-center justify-center h-full space-y-4'>
+      <h1 className='text-red-500 text-4xl font-semibold mb-4'>{t('parrafohabil')}</h1>
+      <div className="flex space-x-4">
         <SkillIcon icon={<FaPython size={100} color={iconColor} />} text="Python" />
         <SkillIcon icon={<FaJava size={100} color={iconColor} />} text="Java" />
         <SkillIcon icon={<FaJs size={100} color={iconColor} />} text="JavaScript" />
@@ -37,7 +37,7 @@ const SkillIcon = ({ icon, text }: { icon: React.ReactNode, text: string }) => {
   return (
     <div className="flex flex-col items-center">
       {icon}
-      <p className="text-md md:text-lg text-red-500 font-roboto font-semibold text-center">{text}</p>
+      <p className="text-lg text-red-500 font-roboto font-semibold">{text}</p>
     </div>
   );
 };
